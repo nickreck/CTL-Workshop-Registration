ae15d10ab0b7c62553443dd75f686f2cc110b25e
kylecarrollCIS <58036707+kylecarrollCIS@users.noreply.github.com>
2022-04-11 10:17:26 -0400

    mostly finished email

--- src/main/java/CTLWorkshop/master/MasterController.java
+++ src/main/java/CTLWorkshop/master/MasterController.java
@@ -32,7 +32,7 @@ public class MasterController {
     @PostMapping("/registrationsubmitted")
     public String viewSubmittedRegistrationPage(@ModelAttribute("attendee") Attendee attendee) {
         attendeeRepo.save(attendee);
-        attendee.send("testingjavaemail36@gmail.com","Pineapplessuck010!", "nicholas.reck@bobcats.gcsu.edu","Email testing","Sorry Nick Forgot to Change the address");
+        attendee.send("testingjavaemail36@gmail.com","Pineapplessuck010!", attendee.getId(),"CTL Workshop Registration", "Thank you, " + attendee.getFirstname() + " for registering for " + attendee.getWorkshopnum() + "You will receive a reminder 24 hrs before the workshop that will include the address/online link.");
         return "registration_submitted";
     }
     @GetMapping("/admin")
